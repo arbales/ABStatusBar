ed6a4630c8906eaeddeea0d6d46ac32cc338cdca
Austin Bales <arbales@gmail.com>
2025-11-12 19:01:40 -0800

    Initial Commit

---
+++ ABStatusBar/ABStatusBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABStatusBarApp.swift
+//  ABStatusBar
+//
+//  Created by Austin Bales on 2025-11-12.
+//
+
+import SwiftUI
+
+@main
+struct ABStatusBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
